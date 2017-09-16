2db902c85df39887e4f001dbf8d7dd54280e1d39
kirwinkk <31506555+kirwinkk@users.noreply.github.com>
2017-09-16 09:01:54 +0800

    Update line-tcr.py

--- line-tcr.py
+++ line-tcr.py
@@ -6,23 +6,23 @@ from datetime import datetime
 import time,random,sys,json,codecs,threading,glob,re
 
 cl = LINETCR.LINE()
-cl.login(token='EkJkx9BcAp9vLxrxk5Z0.3FLXqaAHaqU2vr+bb+uJKa.q198wdC1ICmGEBjCjqevyGY6xFnpCt+oXyaDSwTyuX4=')
+cl.login(qr=True)
 cl.loginResult()
 
 ki = LINETCR.LINE()
-ki.login(token='EkcP6iUI5KMLwmg6eeV8.iarQyHUgknIRni5Recj+Ea.s10yE5eXbUZ5LjTZqJ7v+UOJ6AP5Z7KwcOdaihy1mdg=')
+ki.login(qr=True)
 ki.loginResult()
 
 kk = LINETCR.LINE()
-kk.login(token='EkQmqtjsfrXnqhTWI8r4.WjRz5oW1/nfIsExC/cMSza.FOPtV6Z7BG2Xd1q8Pt4tnvC/atDIDrKP5SKnuaoUKBY=')
+kk.login(qr=True)
 kk.loginResult()
 
 kc = LINETCR.LINE()
-kc.login(token='EkrlaNcDB0FZsM6didP1.qmf+mG4qg58AVj7ARoBTuq.TNqTDoqvlwBk7QNjWohphJUqpTfkz4tZuVizpnYAtck=')
+kc.login(qr=True)
 kc.loginResult()
 
 ky = LINETCR.LINE()
-ky.login(token='EkqLcYvJ0OIKRK4aA7ad.eem+p7zyhJGXULW/YtGNpq.ALQbpKhzUFeOYTj1aByyxdOLZtGLTQ2o8E33vBWzpDc=')
+ky.login(qr=True)
 ky.loginResult()
 
 print "login success"
@@ -73,8 +73,7 @@ helpMessage ="""ℬᎶ戦神BotV6.5.2
 [Time]   現在時間
 部分指令前打[BG1/2/3]可指定kicker動作
 
-此機器作者↓
-"""
+此機器作者↓"""
 KAC=[cl,ki,kk,kc,ky]
 mid = cl.getProfile().mid
 Amid = ki.getProfile().mid
@@ -342,25 +341,6 @@ def bot(op):
                                 
                     except Exception, e:
                         print e
-                if not op.param2 in Bots:
-                  if wait["protectionOn"] == True:  
-                   try:
-                       G = ki.getGroup(op.param1)
-                       ginfo = ki.getGroup
-		       klist=[ki,kk,kc]
-                       G.preventJoinByTicket = False
-                       ki.updateGroup(G)
-                       invsend = 0
-                       Ticket = ki.reissueGroupTicket(op.param1)
-                       ky.acceptGroupInvitationByTicket(op.param1,Ticket)
-                       cl.acceptGroupInvitationByTicket(op.param1,Ticket)
-                       X = cl.getGroup(op.param1)             
-                       X.preventJoinByTicket = True
-                       ky.kickoutFromGroup(op.param1,[op.param2])                       
-		       cl.updateGroup(X)
-                       ky.leaveGroup(op.param1)
-                   except Exception, e:
-                            print e
 
                 if mid in op.param3:
                     if op.param2 in Bots:
@@ -1500,35 +1480,43 @@ def bot(op):
 		else:
                     cl.sendText(msg.to,"權限不足 無法執行")
 		
-            elif "Cleanse" in msg.text:
-                if msg.from_ in admin:
-                    print "ok"
-                    _name = msg.text.replace("Cleanse","")
-                    gs = ki.getGroup(msg.to)
-                    gs = kk.getGroup(msg.to)
-                    gs = kc.getGroup(msg.to)
-                    ki.sendText(msg.to,"ℬᎶ戦神Bot降臨~")
-                    kk.sendText(msg.to,"ℬᎶ戦神Bot降臨~")
-                    kc.sendText(msg.to,"ℬᎶ戦神Bot降臨~")
-                    targets = []
-                    for g in gs.members:
-                        if _name in g.displayName:
-                            targets.append(g.mid)
-                    if targets == []:
-                        ki.sendText(msg.to,"錯誤")
-                        kk.sendText(msg.to,"錯誤")
-                        kc.sendText(msg.to,"錯誤")
-                    else:
-                        for target in targets:
-                            try:
-                                klist=[ki,kk,kc]
-                                kicker=random.choice(klist)
-                                kicker.kickoutFromGroup(msg.to,[target])
-                                print (msg.to,[g.mid])
-                            except:
-                                ki.sendText(msg.to,"Group cleanse")
-                                kk.sendText(msg.to,"Group cleanse")
-                                kc.sendText(msg.to,"Group cleanse")
+		
+            elif msg.text in ["Cleanse","cleanse"]:
+                if msg.toType == 2:
+                    if msg.from_ in admin:
+                        print "[Command]Cleanse executing"
+                        _name = msg.text.replace("Cleanse","")
+                        gs = ki.getGroup(msg.to)
+                        gs = kk.getGroup(msg.to)
+                        gs = kc.getGroup(msg.to)
+			ki.sendText(msg.to,"ℬᎶ戦神Bot降臨~")
+                        kk.sendText(msg.to,"ℬᎶ戦神Bot降臨~")
+                        kc.sendText(msg.to,"ℬᎶ戦神Bot降臨~")
+                        targets = []
+                        for g in gs.members:
+                            if _name in g.displayName:
+                                targets.append(g.mid)
+                        # --------------[Bot and Admin MID]----------------
+                        targets.remove(adminMID)
+                        targets.remove(mid)
+                        targets.remove(Amid)
+                        targets.remove(Bmid)
+                        targets.remove(Cmid)
+                        targets.remove(Lmid)
+                        # --------------[Bot and Admin MID]----------------
+                        if targets == []:
+                            ki.sendText(msg.to,"ℬᎶ戦神Bot降臨~")
+                        else:
+                            for target in targets:
+                                try:
+                                    klist=[ki,kk,kc,cl,kg]
+                                    kicker=random.choice(klist)
+                                    kicker.kickoutFromGroup(msg.to,[target])
+                                    print (msg.to,[g.mid])
+                                except:
+                                    ki.sendText(msg.to,"ℬᎶ戦神Bot降臨~")
+                                    kk.sendText(msg.to,"ℬᎶ戦神Bot降臨~")
+                                    kc.sendText(msg.to,"ℬᎶ戦神Bot降臨~")
 		else:
                     cl.sendText(msg.to,"權限不足 無法執行")
 
