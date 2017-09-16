c3a52d5424fe43a40d692a182960f3a9a8470c2c
kirwinkk <31506555+kirwinkk@users.noreply.github.com>
2017-09-16 12:58:10 +0800

    Update line-tcr.py

--- line-tcr.py
+++ line-tcr.py
@@ -23,9 +23,6 @@ wait = {
 setTime = {}
 setTime = wait["setTime"]
 
-print client._loginresult()
-print "Login successfull"
-
 def sendMessage(to, text, contentMetadata={}, contentType=0):
     mes = Message()
     mes.to, mes.from_ = to, profile.mid
