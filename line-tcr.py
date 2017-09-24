dea43aeff05314e97bdfee456881addbf3ad0d65
kirwinkk <31506555+kirwinkk@users.noreply.github.com>
2017-09-24 22:13:51 +0800

    Update line-tcr.py

--- line-tcr.py
+++ line-tcr.py
@@ -6,156 +6,47 @@ from datetime import datetime
 import time,random,sys,json,codecs,threading,glob,re
 
 cl = LINETCR.LINE()
-cl.login(token="Ek4caEJ8UPCs4iNyxkp5.uEmdFcf6siszYLOn7RH6zq.GzZM3nhElQycHMM3FHoAkrF5H8bYJPKJM7/mLwDZ/jM=")
+cl.login(token="ElpyWnrTQQHjnhAqnGN5.uEmdFcf6siszYLOn7RH6zq.h6129jw528y3MELRp9Q2udXmR1WaQRITyaBvyty+QX4=")
 cl.loginResult()
 
 print "login success"
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-KAC=[cl]
 mid = cl.getProfile().mid
-Bots=[mid]
 
-wait = {
-    'contact':False,
-    'autoJoin':True,
-    'autoCancel':{"on":True,"members":1},
-    'leaveRoom':True,
-    'timeline':True,
-    'autoAdd':True,
-    "lang":"JP",
-    "comment":"ℬᎶ戦神Family",
-    "commentOn":True,
-    "commentBlack":{},
-    "wblack":False,
-    "dblack":False,
-    "blacklist":{},
-    "wblacklist":False,
-    "dblacklist":False,
-    "protectionOn":True,
-    "atjointicket":True
-    }
-
-wait2 = {
-    'readPoint':{},
-    'readMember':{},
-    'setTime':{},
-    'ROM':{}
-    }
-cancelinvite = {
-    'autoCancel':True,
-    'autoCancelUrl':True
-}
-
-setTime = {}
-setTime = wait2["setTime"]
-
-
-def sendMessage(to, text, contentMetadata={}, contentType=0):
-    mes = Message()
-    mes.to, mes.from_ = to, profile.mid
-    mes.text = text
-    mes.contentType, mes.contentMetadata = contentType, contentMetadata
-    if to not in messageReq:
-        messageReq[to] = -1
-    messageReq[to] += 1
-
-def NOTIFIED_READ_MESSAGE(op):
-    #print op
-    try:
-        if op.param1 in wait['readPoint']:
-            Name = client.getContact(op.param2).displayName
-            if Name in wait['readMember'][op.param1]:
-                pass
-            else:
-                wait['readMember'][op.param1] += "\n・" + Name
-                wait['ROM'][op.param1][op.param2] = "・" + Name
-        else:
-            pass
-    except:
-        pass
-
-def RECEIVE_MESSAGE(op):
-    msg = op.message
-    try:
-        if msg.contentType == 0:
-            try:
-                if msg.to in wait['readPoint']:
-                    if msg.from_ in wait["ROM"][msg.to]:
-                        del wait["ROM"][msg.to][msg.from_]
-                else:
-                    pass
-            except:
-                pass
-        else:
-            pass
-    except KeyboardInterrupt:
-	       sys.exit(0)
-    except Exception as error:
-        print error
-        print ("\n\nRECEIVE_MESSAGE\n\n")
-        return
-
-def NOTIFIED_KICKOUT_FROM_GROUP(op):
-    if not op.param2 in ["uc216d8664c4e1f43772c98b1b0b8956e"]:
-	try:
-	   if op.type == 19:
-              klist=[ki,kk,kc,cl,kg]
-              kicker=random.choice(klist)
-              kicker.kickoutFromGroup(msg.to,[target])
-              Glist=[ki,kk,kc,cl,kg]
-              Gicker=random.choice(Glist)
-	      Gicker.inviteIntoGroup(op.param1,[op.param3])
-	except Exception, e:
-	   print 'failed'
 
 def bot(op):
     try:
         if op.type == 0:
             return
-        if op.type == 5:
-            if wait["autoAdd"] == True:
-                cl.findAndAddContactsByMid(op.param1)
-                if (wait["message"] in [""," ","\n",None]):
-                    pass
-                else:
-                    cl.sendText(op.param1,str(wait["message"]))
-                    msg.contentType = 13
-                    msg.contentMetadata = {'mid': "uc216d8664c4e1f43772c98b1b0b8956e"}
-                    cl.sendMessage(msg)
         if op.type == 13:
-           cl.getGroup(op.param1)
-           cl.acceptGroupInvitation(op.param1)
-           X = cl.getGroup(msg.to)
-           if X.invitee is not None:
-             gInviMids = [contact.mid for contact in X.invitee]
-             cl.cancelGroupInvitation(msg.to, gInviMids)
-	     cl.leaveGroup(op.param1)
-    except Exception as e:
-        print e
-        print ("\n\nNOTIFIED_INVITE_INTO_GROUP\n\n")
-        return
+            if mid in op.param3:
+                try:
+                    cl.acceptGroupInvitation(op.param1)
+                    start = time.time()
+                    group = cl.getGroup(op.param1)
+                    if group.invitee is not None:
+                        gInviMids = [contact.mid for contact in group.invitee]
+                        cl.cancelGroupInvitation(op.param1, gInviMids)
+			elapsed_time = time.time() - start
+                        cl.sendText(op.param1,"戦神已取消所有邀請(´∀｀)♡" + "\n耗費時間:%sseconds" % (elapsed_time) + "\n\n戦神BOT作者↓\nhttp://line.me/ti/p/4-ZKcjagH0\n[Made In Taiwan]")
+                    else:
+			elapsed_time = time.time() - start
+                        cl.sendText(op.param1,"戦神發現...邀請中沒人><" + "\n耗費時間:%sseconds" % (elapsed_time) + "\n\n戦神BOT作者↓\nhttp://line.me/ti/p/4-ZKcjagH0\n[Made In Taiwan]")
+                    cl.leaveGroup(op.param1)
+                except:
+                    pass
+            else:
+                pass
+        if op.type == 5:
+                    cl.sendText(op.param1,"戦神BOT作者↓\nhttp://line.me/ti/p/4-ZKcjagH0\n[Made In Taiwan]")
+
+    except Exception as error:
+        #print error
+        pass
 
 
-
-def NOTIFIED_ADD_CONTACT(op):
-    try:
-        sendMessage(op.param1, "ℬᎶ戦神Cancel Bot 作者↓")
-        sendMessage(op.param1, text=None, contentMetadata={'mid': "uc216d8664c4e1f43772c98b1b0b8956e"}, contentType=13)
-    except Exception as e:
-        print e
-        print ("\n\nNOTIFIED_ADD_CONTACT\n\n")
-        return
-
-def NOTIFIED_KICKOUT_FROM_GROUP(op):
-    if not op.param2 in ["uc216d8664c4e1f43772c98b1b0b8956e"]:
-	try:
-	   if op.type == 19:
-	      cl.kickoutFromGroup(op.param1,[op.param2])
-	      cl.inviteIntoGroup(op.param1,[op.param3])
-	except Exception, e:
-	   print 'failed'
 while True:
     try:
         Ops = cl.fetchOps(cl.Poll.rev, 5)
